96c1f8506e1b69c502bf3b7b027a0cd5450a3f7c
Bernhard Wittmann <dev@bernhardwittmann.com>
2019-10-31 17:41:51 +0100

    ci: adjusted Jest Coverage config
    Added coverage collection to jest tests

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,11 @@
 module.exports = {
-  preset: '@vue/cli-plugin-unit-jest/presets/typescript-and-babel'
+  preset: '@vue/cli-plugin-unit-jest/presets/typescript-and-babel',
+  collectCoverage: true,
+  collectCoverageFrom: [
+    'src/**/*.{js,ts,vue}',
+    '!**shims*',
+    '!src/plugins/*',
+    '!src/main.ts'
+  ],
+  coverageReporters: ['text-summary']
 }
