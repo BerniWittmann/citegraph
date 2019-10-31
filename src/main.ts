87d87a60876c1932ce360ebc406a7471e5525eeb
Bernhard Wittmann <dev@bernhardwittmann.com>
2019-10-31 17:02:50 +0100

    feat: Added vue-i18n plugin for i18n
    For multiple language support the vue-i18n library
    (https://github.com/kazupon/vue-i18n) was added

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,13 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import './plugins/element.js'
+import i18n from './plugins/i18n'
 
 Vue.config.productionTip = false
 
 new Vue({
   router,
   store,
+  i18n,
   render: h => h(App)
 }).$mount('#app')
